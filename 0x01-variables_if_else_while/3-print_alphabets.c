315511b1f2f016fe40f806ad8c94943ab9c656f4
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-12 22:55:22 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,12 @@
 int main(void)
 {
         char i;
-
+	
         for (i = 'a'; i <= 'z'; ++i)
 		putchar(i);
-	
         for (i = 'A'; i <= 'Z'; ++i)
 		putchar(i);
-
         putchar('\n');
-
+	
 	return (0);
 }
