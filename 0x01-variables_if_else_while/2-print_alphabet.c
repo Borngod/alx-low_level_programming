0b035dc7b6e323aa8d951ede130753a0772428b9
Borngod <aboa.boni360@gmail>
2022-12-12 15:44:15 -0800

    new one

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
+ * main - A code that prints lowercase letters
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-	int d;
+	char i;
 
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
 	putchar('\n');
-
-	return (0);
-}
+	
+        return (0);
+ }
