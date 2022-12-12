b59551b7517c86b1a1ab82a5498983bb2dfb3d7e
Borngod <aboa.boni360@gmail>
2022-12-12 14:06:13 -0800

    alphabets

---
+++ 3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+  *main:Entry point
+  *
+  *Return:Always 0(success)
+  */
+int main(void)
+{
+	char i;
+
+
+        for(i = 'a';i <= 'z';++i)
+        { 
+        putchar(i);
+
+        }
+
+
+        char b;
+
+
+        for(b = 'A'; b <= 'Z';++b)
+        {
+            putchar(b);
+        }
+
+
+        putchar('\n');
+        putchar('h');
+	return (0);
+}
