7d85d48404208f0eff62192fe5b79395ef149a6d
Borngod <aboa.boni360@gmail>
2022-12-12 19:58:19 -0800

    hello

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (i != 'e' && i != 'q')
+		if (i == 'e' && i != 'q')
 			continue;
-		putchar(i);
+                putchar(i);
 	}
 
 	putchar('\n');
