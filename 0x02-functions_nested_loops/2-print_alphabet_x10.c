91a8d66e27ddf981482d651fc2c37247f9cf4324
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-14 21:16:55 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - a function that prints 10 times the alphabet.
  */
 void print_alphabet_x10(void)
 {
