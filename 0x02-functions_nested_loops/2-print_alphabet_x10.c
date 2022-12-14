e6e76ddb4f529a1d65a8a0606123686d3c2bea40
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-14 21:10:49 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i = 1;
         char letter;
 
         while (i++ < 10)
 	{
-		for (letter = 'a'; letter < 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
         }
