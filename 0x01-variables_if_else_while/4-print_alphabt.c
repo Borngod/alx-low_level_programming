4d0dfc3347296cd08f91da4f3213c304f225e6a1
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-14 12:04:25 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,8 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-<<<<<<< HEAD
+
 		if (i == 'e' && i != 'q')
-=======
-		if (i == 'e' || i == 'q')
->>>>>>> dc930a4cc02bdb2caace84d21edffef8a78f73d0
 			continue;
                 putchar(i);
 	}
