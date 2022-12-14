1e9abbd944919d2957b4cdf02689cbe1c1425c46
Borngod <aboa.boni360@gmail>
2022-12-14 14:55:32 -0800

    hallo

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		_putchar(',');
+		_puthvar(' ');
+		_putchar(49);
+	}
+
+	if (n > 0)
+           {
+                  _putchar(43);
+                   _putchar(',');
+  9                 _puthvar(' ');                 _putchar(49);
+ }
+
+}
