9af94ad1a7cc2c20f16a51c6e7b90acceebc38e6
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-15 18:45:06 +0000

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,13 @@
   */
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-
-		else
-			_putchar(i + '0');
-        }
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
+	}
 
 	_putchar('\n');
 }
