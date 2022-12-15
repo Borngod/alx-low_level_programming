6fb25cce273dd6206c9d6bcbbfcdca66fc8c4208
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-15 18:53:17 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,23 +4,14 @@
   *@n:the number of times the line prints
   *
   */
-void print_diagonal(int n)
+void print_line(int n)
 {
-	int len, space;
+	int len;
 
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-				_putchar(' ');
-			_putchar('\\');
-
-			if (len == n - 1)
-				continue;
-
-			_putchar('\n');
-		}
+			_putchar('_');
 	}
 
 	_putchar('\n');
