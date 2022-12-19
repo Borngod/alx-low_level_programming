060884335b73a6f0b698bc7725399f4d5b742e94
Borngod <aboa.boni360@gmail>
2022-12-19 12:57:30 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_strlen-function that returns the length of a string
+  *@s-the variable containing the string
+  *
+  */
+int _strlen(char *s)
+{
+	_printf("%d\n", _strlen(*s));
+
+}
