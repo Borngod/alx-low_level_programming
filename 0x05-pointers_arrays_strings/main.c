eed5d9b278c119eeadd85c3ff62cbdd129ad448b
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-19 20:08:34 +0000

    Update main.c

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,12 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x04-pointers_arrays_strings directory.
- */
 
 #include <stdlib.h>
 
