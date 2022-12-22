9e9538695a0f8e6c1ced6e8f11ad7b3757454fce
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-22 19:24:28 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,11 @@ char *strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[index++] != '\0')
+	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	for (index = 0; src[index] != '\0'; index++)
-		dest[dest_len++] = src[index];
+	for (index = 0; src[index] != '\0'; index++, dest_len++)
+		dest[dest_len] = src[index];
 
 	return (dest);
 }
