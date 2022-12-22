9d61054cc89f7adb9586ed7f2dbd1b441fa00b85
Borngod <117746948+Borngod@users.noreply.github.com>
2022-12-22 20:11:08 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src,
- *including the terminating
+ * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
@@ -13,13 +12,11 @@ char *strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[dest_len] != '\0')
+	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] != '\0'; index++, dest_len++)
-		dest[dest_len] = src[index];
-
-	dest[dest_len] = '\0'
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
