eafb6f5ee508bc92af6a357a0f6a25ca7e48572b
Borngod <117746948+Borngod@users.noreply.github.com>
2023-01-03 16:16:43 +0000

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,27 @@
-#include "main.h"
+ #include "main.h"
 
 /**
- *_strspn-_strspn
- *@s:a variable
- *@accept: a variable
- *
- *
+ * _strspn - function that gets the length of a prefix substring
+ * @s: given string
+ * @accept: find bytes
+ * Return: Always a.
  */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				break;
+			}
+		}
+		if (accept[b] == '\0')
+			break;
+
+	}
+	return (a);
+}
