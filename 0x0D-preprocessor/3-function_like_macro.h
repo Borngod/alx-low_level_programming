f8f20c6d86c0eb26380286382fd5242e7b206adf
Borngod <117746948+Borngod@users.noreply.github.com>
2023-01-17 05:30:02 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-/*
- * File: 3-function_like_macro.h
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 #endif
