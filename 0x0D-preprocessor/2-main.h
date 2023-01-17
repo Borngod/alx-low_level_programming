ae787b3dd4415a32446fae248b36d9b208e9f778
Borngod <117746948+Borngod@users.noreply.github.com>
2023-01-17 05:24:57 +0000

    Update 2-main.h

--- 0x0D-preprocessor/2-main.h
+++ 0x0D-preprocessor/2-main.h
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - function to print the name of the file it was compiled from
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  *
- * Return: 0 for success
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
